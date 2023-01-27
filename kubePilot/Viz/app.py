8a279c30b2c9dfd4c80a1eb4250c8fe17fae3a82
cudajay <juanjose@smu.edu>
2023-01-26 21:39:02 -0600

    viz with static data

--- kubePilot/Viz/app.py
+++ kubePilot/Viz/app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import pandas as pd
+st.title("Hello from Mr Juan !!!")
+import glob
+import os
 
-def main():
-    st.title("Hello from Mr Juan !!!")
+glb = glob.glob("data/logs/*")
+glb.sort(key=os.path.getmtime, reverse=True)
+chart_data = pd.read_json(glb[0])
 
-if __name__ == '__main__':
-    main()
+st.line_chart(chart_data['global-loss'])
+chart_data = chart_data.drop('global-loss', axis=1)
+st.line_chart(chart_data.filter(regex=("-val")))
+st.line_chart(chart_data.filter(regex=("-loss")))
--- kubePilot/scripts/build_and_deploy_viz.sh
+++ kubePilot/scripts/build_and_deploy_viz.sh
@@ -8,3 +8,5 @@ kubectl -n rabbits delete deployment.apps/viz
 cd ..
 kubectl -n rabbits create -f deployments/viz.yaml
 minikube -n rabbits service test-viz
+sleep 3s
+kubectl -n rabbits port-forward service/test-viz 8000:8000
